10cb52a25e171a329249e9b8597f26d1e3e3f105
André Roberto <32197501+decobeto@users.noreply.github.com>
2019-03-24 23:29:36 -0300

    Adicionado View e Controller

---
+++ TrabalhandoComOVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TrabalhandoComOVisualStudio.Models;
+
+
+namespace TrabalhandoComOVisualStudio.Controllers
+{
+    public class HomeController : Controller {
+        public IActionResult Index()
+        {
+            return View(SimpleRepository.SharedRepository.Products);
+        }
+    }
+}
